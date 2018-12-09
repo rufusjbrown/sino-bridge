759cd08e9c5eb27e6968c9b26524d94a41cbedf5
rufusjbrown <rufusbrown12@gmail.com>
2018-12-09 19:15:01 +0000

    add buefy

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,11 @@ module.exports = {
         })
       }
     }
-  }
+  },
+
+  modules: [
+    // simple usage
+    'nuxt-buefy',
+  ]
 }
 
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,24 +1,37 @@
 <template>
-  <div class="container">
-    <h1 class="title">
-      Contact
-    </h1>
-    <div class="content">
-      <form name="contact" action="" method="post">
-        <label class="form-label" for="name">
-          Name:
-        </label>
-        <input class="form-field" name="name" id="name" />
-        <label class="form-label" for="email">
-          Email:
-        </label>
-        <input class="form-field" name="email" id="email" />
-        <label class="form-label" for="message">
-          Message:
-        </label>
-        <textarea class="form-field" name="message" id="message"></textarea>
-        <input class="form-button" type="submit" value="Send message" />
-      </form>
+    <div class="container is-fluid">
+    <section>
+        <b-field label="Name">
+            <b-input value="Kevin Garvey"></b-input>
+        </b-field>
+
+        <b-field label="Email"
+            type="is-danger"
+            message="This email is invalid">
+            <b-input type="email"
+                value="john@"
+                maxlength="30">
+            </b-input>
+        </b-field>
+
+        <b-field label="Username"
+            type="is-success"
+            message="This username is available">
+            <b-input value="johnsilver" maxlength="30"></b-input>
+        </b-field>
+
+        <b-field label="Password"
+            type="is-warning"
+            :message="['Password is too short', 'Password must have at least 8 characters']">
+            <b-input value="123" type="password" maxlength="30"></b-input>
+        </b-field>
+
+        <b-field label="Subject">
+            <b-select placeholder="Select a subject">
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+            </b-select>
+        </b-field>
+    </section>
     </div>
-  </div>
 </template>
